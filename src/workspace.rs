009aa2729fa87b0a7311fb88f9fbbe04e0cbd89e
s.mozhaev <mozhaev.dev@gmail.com>
2026-09-07 21:53:45 +0800

    add config

--- src/workspace.rs
+++ src/workspace.rs
@@ -1,10 +1,27 @@
+use serde::{Deserialize, Serialize};
 use std::{fs, io, path::Path};
+const CURRENT_SCHEMA_VERSION: u32 = 1;
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+struct WorkspaceConfig {
+    schema_version: u32,
+}
 
 pub fn initialize(path: &Path) -> io::Result<()> {
     fs::create_dir_all(path)?;
     fs::create_dir_all(path.join("data"))?;
     fs::create_dir_all(path.join("episodes"))?;
 
+    let config_path = path.join("siftwave.toml");
+
+    if !config_path.exists() {
+        let config = WorkspaceConfig {
+            schema_version: CURRENT_SCHEMA_VERSION,
+        };
+
+        let content = toml::to_string_pretty(&config).map_err(io::Error::other)?;
+        fs::write(config_path, content)?;
+    }
     Ok(())
 }
 
